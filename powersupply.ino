a4737a08a875c679ef358d034a861c68aa53a925
Marcel Wagner <marcel_wagner_@arcor.de>
2018-04-21 21:52:07 +0200

    More Testing with the ASC712
    got the Current reading pretty acurate ~10mA

--- powersupply.ino
+++ powersupply.ino
@@ -1,12 +1,18 @@
 #include <Wire.h>
 #include <LiquidCrystal_I2C.h>
 
-const int analogIn = A0; 
-int mVperAmp = 185; 
-int RawValue= 0; 
-int ACSoffset = 2500;  
-double Voltage = 0; 
-double Amps = 0; 
+#define analogIn  A1
+#define inputVoltage A0
+
+#define mVperAmp 186 
+#define sensdiff 39
+
+int readValue = 0; 
+
+float sense = 0;
+float vcc = 0; 
+float voltage = 0; 
+float ampere = 0; 
 
 uint8_t block[9]  = {0xaa, 0xaa, 0xaa, 0xaa, 0xaa, 0xaa, 0xaa, 0xaa};
 
@@ -26,9 +32,15 @@ void setup() {
  
 void loop() { 
   // put your main code here, to run repeatedly: 
-  RawValue = analogRead(analogIn); 
-  Voltage = (RawValue / 1024.0) * 5000; // Gets you mV 
-  Amps = ((Voltage - ACSoffset) / mVperAmp); 
+  readValue = analogRead(analogIn); 
+  
+  vcc = (float) 3.30 / analogRead(inputVoltage) * 1023.0;
+  voltage = (float) readValue * vcc / 1023; 
+  voltage = (float) voltage - (vcc/2);
+  sense = (float) mVperAmp - ((5.00-vcc)*sensdiff); 
+  ampere = (float)(voltage * sense)/1000; 
+
+  
   lcd.clear();
   lcd.print("Volt: ");
   if(analogIn != 512){
@@ -37,15 +49,20 @@ void loop() {
   lcd.setCursor(0,1);
   lcd.print("Ampere: ");
   if(analogRead(analogIn) != 512){
-    lcd.print(Amps);
+    lcd.print(ampere);
   }
   lcd.print("A");
   
   Serial.print("Raw Value = " ); // shows pre-scaled value  
-  Serial.print(RawValue);  
+  Serial.print(readValue);  
   Serial.print("\t mV = "); // shows the voltage measured  
-  Serial.print(Voltage,3); // the '3' after voltage allows you to display 3 digits after decimal point 
+  Serial.print(voltage,3); // the '3' after voltage allows you to display 3 digits after decimal point 
   Serial.print("\t Amps = "); // shows the voltage measured  
-  Serial.println(Amps,3); // the '3' after voltage allows you to display 3 digits after decimal point 
+  Serial.print(ampere,3); // the '3' after voltage allows you to display 3 digits after decimal point
+  Serial.print("\t Vcc = ");
+  Serial.print(vcc);
+  Serial.print("\t Sense = ");
+  Serial.print(sense);
+  Serial.println(" ");
   delay(2500); 
 } 
